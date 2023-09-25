bb9a1dd731f31eccb7cdf28f1dc8dd9c3004c59f
Alfaz Hossain <alfaz.cse@gmail.com>
2023-09-25 12:48:54 +0600

    sweetalert is added for new data and duplicate data

--- src/Pages/CardDetails.jsx/SingleCardDetails.jsx
+++ src/Pages/CardDetails.jsx/SingleCardDetails.jsx
@@ -1,7 +1,38 @@
 
+import swal from "sweetalert";
 
 const SingleCardDetails = ({singleCard}) => {
+
+
   const {category,color_card_bg,color_category_bg,color_text_and_btn_bg,description,id,image,price,title} =singleCard;
+ 
+  const handleAdded =() =>{
+    const addedToDonateList = [];
+     
+    const donateItems =JSON.parse(localStorage.getItem("donation"))
+
+    if(!donateItems){
+      addedToDonateList.push(singleCard);
+      localStorage.setItem("donation",JSON.stringify(addedToDonateList))
+      swal("Good job!", "Donation added successfully!", "success");
+    }
+    else{
+       const isExists = donateItems.find(card=>card.id ===id)
+     if(!isExists){
+      addedToDonateList.push(...donateItems,singleCard)
+      localStorage.setItem("donation",JSON.stringify(addedToDonateList));
+      swal("Good job!", "Donation added successfully!", "success");
+     }
+     else{
+      swal("Error!", "No duplicate !", "error");
+     }
+    }
+  }
+  
+
+
+
+
 
   return (
     <div>
@@ -9,8 +40,8 @@ const SingleCardDetails = ({singleCard}) => {
       <img  src={image} alt="Your Image" className="w-full h-[65vh]" />
 
      <div className="absolute inset-x-0 bottom-0 bg-black bg-opacity-50 text-left p-4">
-    <button className=" text-white font-bold py-2 px-4 rounded " style={{ backgroundColor: color_text_and_btn_bg}}>
-      Click Me
+    <button onClick={handleAdded} className=" text-white font-bold py-2 px-4 rounded " style={{ backgroundColor: color_text_and_btn_bg}}>
+      Donate ${price}
     </button>
   </div>
 </div>
